4bf9c84025305fd88de6065f3966861a5e3ce9f8
r3nniw <r3nniw@r3nniw-15.local>
2020-12-02 01:50:10 +0700

    simple GraphQL server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,32 @@
-console.log('hello world')
+import { ApolloServer, gql } from 'apollo-server'
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      books: [Book]
+    }
+
+    type Book {
+      title: String
+      author: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      books: () => [
+        {
+          title: 'The Awakening',
+          author: 'Kate Chopin'
+        },
+        {
+          title: 'City of Glass',
+          author: 'Paul Auster'
+        }
+      ]
+    }
+  }
+})
+
+server.listen({ port: 3000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+})
