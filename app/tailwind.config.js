1373d2b517641e9727c7dc653e7c93a3823dfde2
krikchaip <krikchai.p@gmail.com>
2021-06-28 00:46:10 +0700

    🎉 feat(app): installed official tailwind plugins

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -17,5 +17,9 @@ module.exports = {
   variants: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio')
+  ]
 }
