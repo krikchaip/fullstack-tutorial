e25023bc5d717b844f1c262e31d2c223334fc1c5
krikchaip <krikchai.p@gmail.com>
2021-08-30 01:53:17 +0700

    🎉 feat: add navbar to app layout

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,8 +2,17 @@ import 'tailwindcss/tailwind.css'
 
 import type { AppProps } from 'next/app'
 
+import { Navbar } from 'lib/components'
+
 export function App({ Component: Page, pageProps }: AppProps) {
-  return <Page {...pageProps} />
+  return (
+    <div className="w-screen h-screen flex flex-col">
+      <Navbar className="px-4" />
+      <div className="p-4 flex-1 overflow-x-hidden overflow-y-scroll">
+        <Page {...pageProps} />
+      </div>
+    </div>
+  )
 }
 
 export default App
