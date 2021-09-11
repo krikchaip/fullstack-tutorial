5c2c81dbf8e634afc5f27583865e35d03f8d0374
krikchaip <krikchai.p@gmail.com>
2021-09-12 02:54:59 +0700

    🎉 feat(app): input component

--- app/lib/components/input.tsx
+++ app/lib/components/input.tsx
@@ -1,13 +1,41 @@
-import { DetailedHTMLProps, InputHTMLAttributes } from 'react'
+import { DetailedHTMLProps, forwardRef, InputHTMLAttributes } from 'react'
+import classNames from 'classnames'
 
 import { overrideStyles } from 'lib/utils'
+
+import { Icon } from './icon'
 
 export type InputProps = DetailedHTMLProps<
   InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
->
+> & {
+  /** refer to svg icon filename inside `/pubilc/icon` */
+  icon?: string
 
-export function Input(props: InputProps) {
-  const { c, s, ...otherProps } = overrideStyles(props)
-  return <input className={c()} style={s()} {...otherProps} />
+  /** error message */
+  error?: string
 }
+
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const { c, s, icon, error, ...otherProps } = overrideStyles(props)
+  return (
+    <label
+      className={c(
+        'inline-block relative text-[#828282] leading-[1.375rem] rounded-lg border-solid border border-[#BDBDBD]'
+      )}
+      style={s()}
+    >
+      {icon && (
+        <Icon className="absolute top-3 left-3 -translate-y-px" name={icon} />
+      )}
+      <input
+        className={classNames(
+          'w-full p-3 text-[color:inherit] leading-[inherit] rounded-[inherit] border-none transition placeholder-current',
+          { 'pl-11': !!icon }
+        )}
+        ref={ref}
+        {...otherProps}
+      />
+    </label>
+  )
+}) as (props: InputProps) => JSX.Element
