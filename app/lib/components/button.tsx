e8069a20dd1a935a225cdf6bf9ee25caac9d9a44
krikchaip <krikchai.p@gmail.com>
2021-09-13 02:45:36 +0700

    🎉 feat(app): button component

--- app/lib/components/button.tsx
+++ app/lib/components/button.tsx
@@ -5,9 +5,27 @@ import { overrideStyles } from 'lib/utils'
 export type ButtonProps = DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
->
+> & {
+  /** @default 'solid'*/
+  variant?: 'solid' | 'outline'
+}
 
 export function Button(props: ButtonProps) {
-  const { c, s, ...otherProps } = overrideStyles(props)
-  return <button className={c()} style={s()} {...otherProps} />
+  const { c, s, variant = 'solid', ...otherProps } = overrideStyles(props)
+  return (
+    <button
+      className={c(
+        'h-10',
+        'flex items-center justify-center',
+        'text-center',
+        variant === 'solid' && 'text-white',
+        'rounded-lg',
+        'hover:bg-opacity-80 active:bg-opacity-95',
+        'active:ring-2 focus-visible:outline-none focus-visible:ring-2',
+        'transition'
+      )}
+      style={s()}
+      {...otherProps}
+    />
+  )
 }
