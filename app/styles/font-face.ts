32d49378e8a2674a66aaa15f383fa463ab500fc4
krikchaip <krikchai.p@gmail.com>
2021-10-19 02:45:20 +0700

    🧹 chore: add more NotoSans font-faces

--- app/styles/font-face.ts
+++ app/styles/font-face.ts
@@ -1,7 +1,14 @@
 import { css } from 'twin.macro'
 
-// TODO: NotoSans 300, NotoSans 500, NotoSans 600
+// TODO: NotoSans 600
 const notosans = css`
+  @font-face {
+    font-family: NotoSans;
+    font-weight: 300;
+    font-style: normal;
+    src: url('/font/NotoSans/NotoSans-Light.ttf');
+  }
+
   @font-face {
     font-family: NotoSans;
     font-weight: 400;
@@ -14,6 +21,20 @@ const notosans = css`
     font-weight: 400;
     font-style: italic;
     src: url('/font/NotoSans/NotoSans-Italic.ttf');
+  }
+
+  @font-face {
+    font-family: NotoSans;
+    font-weight: 500;
+    font-style: normal;
+    src: url('/font/NotoSans/NotoSans-Medium.ttf');
+  }
+
+  @font-face {
+    font-family: NotoSans;
+    font-weight: 600;
+    font-style: normal;
+    src: url('/font/NotoSans/NotoSans-SemiBold.ttf');
   }
 
   @font-face {
