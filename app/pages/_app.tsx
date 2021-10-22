28851ac4e15dc2bb38b7a2774d005b9574d7eb46
krikchaip <krikchai.p@gmail.com>
2021-10-23 03:31:11 +0700

    🚑 fix: remove navbar on 404 page

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -4,11 +4,18 @@ import { GlobalStyles } from 'styles'
 import { Navbar } from 'lib/components'
 
 export function App({ Component: Page, pageProps }: AppProps) {
+  const error = pageProps?.statusCode === 404
+  const shouldDisplayNavbar = true
+
+  if (error) {
+    return <Page {...pageProps} />
+  }
+
   return (
     <>
       <GlobalStyles />
       <div tw="w-screen h-screen flex flex-col">
-        <Navbar tw="px-4" />
+        {shouldDisplayNavbar && <Navbar tw="px-4" />}
         <div tw="p-4 flex-1 overflow-x-hidden overflow-y-scroll">
           <Page {...pageProps} />
         </div>
