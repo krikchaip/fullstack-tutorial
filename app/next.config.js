bd7a9c7129f1c13c811089d974dd9cfcc3588f63
krikchaip <krikchai.p@gmail.com>
2022-01-17 03:25:44 +0700

    nextjs import svg as a component support

--- app/next.config.js
+++ app/next.config.js
@@ -8,5 +8,15 @@ module.exports = {
     dirs: Object.keys(tsconfig.compilerOptions.paths).map(path =>
       path.replace('/*', '')
     )
+  },
+  webpack: config => {
+    // let you import SVG as a React component
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    })
+
+    return config
   }
 }
